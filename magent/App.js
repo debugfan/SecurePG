10fe8ee0d98f73cbf2b40af8586c88ca77a978ca
debugfanchin <debugfanchin@gmail.com>
2018-03-20 13:32:14 +0800

    implement basic feature of mobile agent

--- magent/App.js
+++ magent/App.js
@@ -6,11 +6,20 @@
 
 import React, { Component } from 'react';
 import {
+  Alert,
+  Button, 
+  TextInput,
   Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+import { JSEncrypt } from 'jsencrypt';
+var RNFS = require('react-native-fs');
+var CryptoJS = require("crypto-js");
+var jsrsasign = require("jsrsasign");
+var net = require('react-native-tcp');
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,20 +28,204 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+function decrypt_key(secret, password, enckey, label) {
+  // Decrypt private key using passphrase
+  decPKHex = jsrsasign.KEYUTIL.getDecryptedKeyHex(secret, password);
+  // Convert to PEM format for JSEncrypt
+  decPKPEM = jsrsasign.KJUR.asn1.ASN1Util.getPEMStringFromHex(decPKHex, "RSA PRIVATE KEY");
+  
+  // Decrypt with the private key...
+  var decrypt = new JSEncrypt();
+  decrypt.setPrivateKey(decPKPEM);
+  var uncrypted = decrypt.decrypt(enckey);
+  var dek = CryptoJS.HmacSHA256(label, uncrypted);
+  
+  return dek;
+}
+
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {url: "http://192.168.0.100/example.key", port: "9600", server: null, secret: "", password: "123456", started: false};
+
+    this.importFile = this.importFile.bind(this);
+    this.removeFile = this.removeFile.bind(this);
+    this.startService = this.startService.bind(this);
+    this.stopService = this.stopService.bind(this);
+  }
+   
+  importFile() {
+    var url = this.state.url;
+
+    if(url == "") {
+      Alert.alert('Error', 'No URL specified!');
+      return;
+    }
+
+    fetch(url)
+      .then((response) => response.text())
+      .then((body) => {
+        var path = RNFS.DocumentDirectoryPath + '/secret.key';
+        // write the file
+        console.log('Writing file: ', path, body);
+        RNFS.writeFile(path, body)
+        .then((success) => {
+          Alert.alert('Information', 'Import successfully!');
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  removeFile() {
+    // create a path you want to delete
+    var path = RNFS.DocumentDirectoryPath + '/secret.key';
+
+    RNFS.unlink(path)
+      .then(() => {
+        console.log('FILE DELETED');
+        Alert.alert('Information', 'Delete successfully!');
+      })
+      // `unlink` will throw an error, if the item to unlink does not exist
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  start_agent_server(port, secret, password) {
+    var port = parseInt(port);
+    console.log('listen on port: ', port);
+
+    var srv = net.createServer(function(socket) {
+      
+      socket.secureRecv = function(data) {
+        if (typeof(socket.sessionKey) == "undefined") {
+          var session = JSON.parse(data);
+          socket.sessionKey = decrypt_key(secret, password, session.key, session.label);
+          payload = CryptoJS.AES.decrypt(session.data, socket.sessionKey, {mode: CryptoJS.mode.ECB});
+          return payload.toString(CryptoJS.enc.Latin1);
+        } else {
+          payload = CryptoJS.AES.decrypt(data, socket.sessionKey, {mode: CryptoJS.mode.ECB});
+          return payload.toString(CryptoJS.enc.Latin1);
+        }
+      }
+
+      socket.secureSend = function(data) {
+        var enc_data = CryptoJS.AES.encrypt(data, socket.sessionKey, {mode: CryptoJS.mode.ECB});
+        socket.write(enc_data.toString());
+      }
+
+      socket.on('data', function(data) {
+        var app_data = socket.secureRecv(data);     
+        var app_json = JSON.parse(app_data);
+        Alert.alert(
+          'Decrypt Request',
+          'Label: ' + app_json.label,
+          [
+            {text: 'Accept', onPress: () => {
+              console.log('Decrypt request accepted.');
+              var dek = decrypt_key(secret, password, app_json.key, app_json.label);
+              socket.secureSend(dek.toString(CryptoJS.enc.Base64));
+            }},
+            {text: 'Reject', style: 'cancel', onPress: () => {
+              console.log('Cancel Pressed');
+              socket.close();
+            }}
+          ],
+          { cancelable: false }
+        )
+      });
+    
+      // Add a 'close' event handler to this instance of socket
+      socket.on('close', function(data) {
+         console.log('Connection closed.');
+      });
+    }).listen(port);
+
+    this.setState({server: srv});
+    this.setState({started: true});
+  }
+
+  startService() {
+    if(this.state.password == "") {
+      Alert.alert('Warning', 'No password set!');
+    }
+
+    var password = this.state.password;
+    var port = parseInt(this.state.port);
+
+    var path = RNFS.DocumentDirectoryPath + '/secret.key';
+    console.log('secret file path: ', path);
+
+    RNFS.readFile(path, 'utf8')
+      .then((contents) => {
+        this.start_agent_server(port, contents, password);
+      })
+      .catch((err) => {
+        console.log(err.message, err.code);
+      });
+  }
+
+  stopService() {
+    if(this.state.server != null) {
+      this.state.server.close();
+    }
+    
+    this.setState({started: false});
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.row}>
+          <Text>URL: </Text>
+          <TextInput
+            value={this.state.url}
+            onChangeText={(text) => this.setState({url: text})}
+          />
+        </View>
+        <View style={styles.row}>
+          <Button
+            onPress={this.importFile}
+            title="Import"
+          />
+          <Button
+            onPress={this.removeFile}
+            title="Remove"
+          />
+        </View>
+        <View style={styles.row}>
+          <Text>Password: </Text>
+          <TextInput
+            secureTextEntry={true}
+            value={this.state.password}
+            onChangeText={(text) => this.setState({password: text})}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text>Port: </Text>
+          <TextInput
+            value={this.state.port}
+            onChangeText={(text) => this.setState({port: text})}
+          />
+        </View>
+        <View style={styles.row}>
+          <Button
+            onPress={this.startService}
+            title="startService"
+            disabled={this.state.started}
+          />
+          <Button
+            onPress={this.stopService}
+            title="stopService"
+            disabled={!this.state.started}
+          />
+        </View>
       </View>
     );
   }
@@ -45,14 +238,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
---
+++ magent/start_android.bat
@@ -0,0 +1,7 @@
+setlocal
+
+if not exist "android\app\src\main\assets" (
+  mkdir android\app\src\main\assets
+)
+
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res && react-native run-android
